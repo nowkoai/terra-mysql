ac48892413dae7fd00a47446774946ceadf9c0e2
nowkoai <hyamasit@gmail.com>
2022-03-14 18:02:10 +0900

    sql

---
+++ mysql.tf
@@ -0,0 +1,23 @@
+terraform {
+  # required_version = "0.13.4"
+
+  required_providers {
+
+    mysql = {
+      source  = "terraform-providers/mysql"
+      version = "1.9.0"
+    }
+  }
+}
+
+# Configure the MySQL provider
+provider "mysql" {
+  endpoint = "localhost:3306"
+  username = "root"
+  password = "Yama_33783378"
+}
+
+# Create a Database
+resource "mysql_database" "app" {
+  name = "nowko_app22"
+}
